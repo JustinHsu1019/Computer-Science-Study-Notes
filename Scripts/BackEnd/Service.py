2446d69cc812cf6b23f1b66022bf086e7b286521
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-11-05 21:13:21 +0800

    Update FlaskCors

--- Scripts/BackEnd/Service.py
+++ Scripts/BackEnd/Service.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from PPT_2_Text import process_pptx
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
